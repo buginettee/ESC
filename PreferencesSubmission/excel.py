705e607938521c01f00d9a76522aea5ad9f0141e
jinghan1997 <41939765+jinghan1997@users.noreply.github.com>
2020-04-02 00:37:21 +0800

    Add files via upload
    Made an executable file for more automation and huge convenience

--- PreferencesSubmission/excel.py
+++ PreferencesSubmission/excel.py
@@ -1,14 +1,17 @@
 import utils
 
 #to access the online excel sheet
-excel_sheet = utils.access_excel()
+file_to_read = 'Capstone groups preferences (Responses)'
+excel_sheet = utils.access_excel(file_to_read)
 #key in the total number of groups foor the year
-total_groups_number = 4
+#total_groups_number = 4
+total_groups_number = int(input("How many groups are there this year: "))
 #check for correct submissions count
 utils.total_submission_checker(total_groups_number, excel_sheet)
 #check for duplicate in submissions
 utils.duplicate_checker(total_groups_number, excel_sheet)
 
+input("Enter anything to exit")
 
 
     
