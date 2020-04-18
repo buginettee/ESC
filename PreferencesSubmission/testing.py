98fc23b4f0fbf70128f7eb9098c544f235f8dbf7
jinghan1997 <41939765+jinghan1997@users.noreply.github.com>
2020-04-18 20:38:38 +0800

    Update testing

--- PreferencesSubmission/testing.py
+++ PreferencesSubmission/testing.py
@@ -52,12 +52,14 @@ class Test(unittest.TestCase):
 
     def test_ts_no_issue(self):
         #test total_submission_checker() with proper inputs, expect to have no issues
+        total_groups_number = 4     #change this line if the total number of groups is updated
         excel_sheet = utils.access_excel('Capstone groups preferences (Responses)')
-        self.assertEqual(utils.total_submission_checker(4, excel_sheet), "End of total_submission_checker", "No issues")
+        self.assertEqual(utils.total_submission_checker(total_groups_number, excel_sheet), "End of total_submission_checker", "No issues")
     def test_d_no_issue(self):
         #test duplicate_checker() with proper inputs, expect to have no issues
+        total_groups_number = 4     #change this line if the total number of groups is updated
         excel_sheet = utils.access_excel('Capstone groups preferences (Responses)')
-        self.assertEqual(utils.duplicate_checker(4, excel_sheet), "End of duplicate_checker", "No issues")
+        self.assertEqual(utils.duplicate_checker(total_groups_number, excel_sheet), "End of duplicate_checker", "No issues")
 
 
 
