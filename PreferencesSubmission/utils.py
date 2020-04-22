f40c0a28b849deb019a77177fe235ada03706606
jinghan1997 <41939765+jinghan1997@users.noreply.github.com>
2020-04-22 13:12:55 +0800

    added edge case and wait time

--- PreferencesSubmission/utils.py
+++ PreferencesSubmission/utils.py
@@ -42,7 +42,7 @@ def total_submission_checker(total_groups_number, sheet):
     else:
         print("All groups have submitted, please proceed")
 
-    return "End of total_submission_checker"
+    return "End of total submission checker"
 
 
 def duplicate_checker(total_groups_number, sheet):
@@ -54,27 +54,29 @@ def duplicate_checker(total_groups_number, sheet):
 
     number_of_submissions = len(sheet.get_all_values()) - 1
 
-    
-    groups_checked = ["Not check"]
-    for i in range(total_groups_number):
-        groups_checked.append("Not check")
+    if (total_groups_number<number_of_submissions):
+        print("Incomplete submissions, unable to check for duplicate")
+    else:
+        groups_checked = ["Not check"]
+        for i in range(total_groups_number):
+            groups_checked.append("Not check")
 
-    duplicate = 0
+        duplicate = 0
 
-    for i in range(2, number_of_submissions + 2):
-        #to extract a particular cell value  row, column
-        team_ids = sheet.cell(i, 2).value
-        #print(team_ids)
-        team_id = int(team_ids)
-        
-        
-        if (groups_checked[team_id] == "Not check"):
-            groups_checked[team_id] = "checked"
-        else:
-            duplicate += 1
-            print("Duplicate found: Team {}".format(team_id))
+        for i in range(2, number_of_submissions + 2):
+            #to extract a particular cell value  row, column
+            team_ids = sheet.cell(i, 2).value
+            #print(team_ids)
+            team_id = int(team_ids)
             
-    if (duplicate == 0):
-        print("No duplicate found")
+            
+            if (groups_checked[team_id] == "Not check"):
+                groups_checked[team_id] = "checked"
+            else:
+                duplicate += 1
+                print("Duplicate found: Team {}".format(team_id))
+                
+        if (duplicate == 0):
+            print("No duplicate found")
 
-    return "End of duplicate_checker"
+    return "End of duplicate checker"
